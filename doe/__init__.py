8cf3179f2d8030a7d90312de6299a62836f3891e
Osburg <aaron.osburg@basf.com>
2022-04-27 11:49:50 +0200

    updated __init__.py

--- doe/__init__.py
+++ doe/__init__.py
@@ -1,1 +1,8 @@
+# flake8: noqa
+
 __version__ = "0.1"
+
+from doe import JacobianForLogdet, design, utils
+from doe.design import find_local_max_ipopt
+from doe.JacobianForLogdet import JacobianForLogdet
+from doe.utils import get_formula_from_string, n_zero_eigvals
