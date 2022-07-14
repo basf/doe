77fee206611c8f461359475edcab2423e0b042c3
Dominik Linzner <linznedd@basfad.basf.net>
2022-07-14 11:26:27 +0200

    - remove outdated import

--- doe/__init__.py
+++ doe/__init__.py
@@ -5,4 +5,4 @@ __version__ = "0.1"
 from doe import design, jacobian, utils
 from doe.design import find_local_max_ipopt
 from doe.jacobian import JacobianForLogdet
-from doe.utils import get_formula_from_string, n_zero_eigvals
+from doe.utils import n_zero_eigvals
